ce33dc1af59f0f77d8dd201c21c0f524eb3889fe
AKarlash <alenakarlash@gmail.com>
2023-10-02 12:31:45 +0300

    “message”

--- good.py
+++ good.py
@@ -16,3 +16,17 @@ digitals = [1,2,3,4,5,6,7,8,9]
 print(max(digitals))
 print(min(digitals))
 print(sum(digitals))
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[-3:])
+print("There is the list of my team")
+for player in players[:3]:
+    print(player.title())
+my_foods = ['pizza', 'carrot', 'soup', 'salad'] 
+friend_food = my_foods[:]  
+my_foods.append('ice cream')
+friend_food.append('lemonad')
+print("My favorite foods are:")
+print(my_foods)
+print("\nMy friend's foods are:")
+print(friend_food)
+
